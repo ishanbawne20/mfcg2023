e45a65f013f4435d197dbfaecfb113e8bd1b3c09
Shubham <skgenius0001@gmail,com>
2023-05-20 15:39:13 +0530

    adding assignment file

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
+// this code consist of a const vector3 v1(1, 2,3 )
+//another vector3 is the variable vector Vector3 v2(x2,y2,z2),variable n2 is the factor by which v2 is scaled or de scaled 
+//charecter c is for the operator
+//input are in order x2,y2,z2,n2,c
+// c='.'(dot product between v1 and v2),c='*'(cross product),c='|'(magnitude of variable vector3)
+//c='+'(addition of v1 and v2),c='-'(subraction of v1 and v2),c='D'(descaling of v2 by n2),c='M'(scaling of v2 by n2)
 #include <iostream>
 #include "vector.h"
 #include<cmath>
