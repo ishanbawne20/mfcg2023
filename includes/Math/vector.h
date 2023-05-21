cdc9c416900ce031a68c776fe10caec3ae715780
ChinmayPillai <113179689+ChinmayPillai@users.noreply.github.com>
2023-05-21 22:54:49 +0530

    Update vector.h

--- includes/Math/vector.h
+++ includes/Math/vector.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <math.h>
 
 class Vector3
 {
@@ -23,7 +24,56 @@ class Vector3
         std::cout<< "X: " << this->x <<" Y: "<< this->y <<" Z: "<< this->z <<std::endl;
     }
 
-    // Start Your Assignment Here.
+    double length(){
+        return sqrt(this->x*this->x + this->y*this->y + this->z*this->z);
+    }
 
+    Vector3 operator+(Vector3 v){
+        Vector3 ans(this->x + v.x, this->y + v.y, this->z + v.z);
+        return ans;
+    }
+
+    Vector3 operator-(Vector3 v){
+        Vector3 ans(this->x - v.x, this->y - v.y, this->z - v.z);
+        return ans;
+    }
+
+    Vector3 operator*(float p){
+        this->x *= p;
+        this->y *= p;
+        this->z *= p;
+        return *this;
+    }
+    
+    double operator*(Vector3 v){
+        double ans = 0;
+        ans += this->x * v.x;
+        ans += this->y * v.y;
+        ans += this->z * v.z;
+        return ans;
+    }
+
+    Vector3 operator/(float p){
+        this->x /= p;
+        this->y /= p;
+        this->z /= p;
+        return *this;
+    }
+
+    double dot(Vector3 v){
+        double ans = 0;
+        ans += this->x * v.x;
+        ans += this->y * v.y;
+        ans += this->z * v.z;
+        return ans;
+    }
+
+    Vector3 cross(Vector3 v){
+        Vector3 ans;
+        ans.x = this->y * v.z - this->z * v.y;
+        ans.y = this->z * v.x - this->x * v.z;
+        ans.z = this->x * v.y - this->y * v.x;
+        return ans;
+    }
 };
 
