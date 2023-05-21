4c1d5af20f4d996f791559eafcb8cb2f5d5a4c8c
Ojas Brid <ojasbc21@iitk.ac.in>
2023-05-22 01:18:29 +0530

    Assignment 1

--- includes/Math/vector.h
+++ includes/Math/vector.h
@@ -24,6 +24,47 @@ class Vector3
     }
 
     // Start Your Assignment Here.
+    
+    double length()
+    {
+        double l;
+        l = this->x * this->x + this->y * this->y + this->z * this->z;
+        l = sqrt(l);
 
+        return l;
+    }
+
+    Vector3 Cross_Product(Vector3 V1, Vector3 V2)
+    {
+        Vector3 cross;
+        
+        cross.x = V1.y * V2.z - V1.z * V2.y;
+        cross.y = V1.z * V2.x - V1.x * V2.z;
+        cross.z = V1.x * V2.y - V1.y * V2.x;
+        return cross;
+    }
 };
+Vector3 operator*(Vector3 const& V1, float const& r)
+{
+    return Vector3(V1.x * r, V1.y * r, V1.z * r);
+}
+float operator*(Vector3 const& V1, Vector3 const& V2)
+{
+    return float(V1.x * V2.x + V1.y * V2.y + V1.z * V2.z);
+}
+
+Vector3 operator+(Vector3 const& V1, Vector3 const& V2)
+{
+    return Vector3(V1.x + V2.x, V1.y + V2.y, V1.z + V2.z);
+}
+
+Vector3 operator-(Vector3 const& V1, Vector3 const& V2)
+{
+    return Vector3(V1.x - V2.x, V1.y - V2.y, V1.z - V2.z);
+}
+
+Vector3 operator/(Vector3 const& V1, float const& r)
+{
+    return Vector3(V1.x/r, V1.y/r, V1.z/r);
+}
 
--- main.cpp
+++ main.cpp
@@ -6,7 +6,27 @@ int main()
     std::cout<<"Assignment One Evaluation"<<std::endl;
 
     Vector3 vec1(1,2,3);
+    Vector3 vec2(1, 5, 3);
+    float scale = 2.3f;
     vec1.print();
+    double length = vec1.length();
+    Vector3 vec3 = vec1 * scale;
+    float vec4 = vec1 * vec2;
+    Vector3 vec5 = vec5.Cross_Product(vec1, vec2);
+    Vector3 vec6 = vec1 + vec2;
+    Vector3 vec7 = vec1 - vec2;
+    Vector3 vec8 = vec1 / scale;
+
+
+    std::cout << length << std::endl;
+    vec3.print();
+    std::cout << vec4 << std::endl;
+    vec5.print();
+    vec6.print();
+    vec7.print();
+    vec8.print();
+
+
     std::cin.get();
     return 0;
 }
