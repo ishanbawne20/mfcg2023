7f5fba493eebd583572470a3c99a3652e2ea1141
Adviji <95600069+Adviji@users.noreply.github.com>
2023-05-23 17:43:50 +0530

    Update vector.h

--- includes/Math/vector.h
+++ includes/Math/vector.h
@@ -24,6 +24,45 @@ class Vector3
     }
 
     // Start Your Assignment Here.
-
+ Vector3 operator + (Vector3 v2){
+        Vector3 v3(0,0,0);
+        v3.x = this->x + v2.x;
+        v3.y = this->y + v2.y;
+        v3.z = this->z + v2.z;
+        return v3;
+    }
+    Vector3 operator - (Vector3 v2){
+        Vector3 v3(0,0,0);
+        v3.x = this->x - v2.x;
+        v3.y = this->y - v2.y;
+        v3.z = this->z - v2.z;
+        return v3;
+    }
+    Vector3 operator / (float k){
+        Vector3 v3;
+        v3.x = this->x/k;
+        v3.y = this->y/k;
+        v3.z = this->z/k;
+        return v3;
+    }
+    Vector3 operator * (Vector3 v2){
+        Vector3 v3(0,0,0);
+        v3.x = this->x*v2.x;
+        v3.y = this->y*v2.y;
+        v3.z = this->z*v2.z;
+        return v3;
+    }
+    void print_dot(){
+        float sum = x+y+z;
+        std::cout<<sum<<std::endl;
+    }
+    //cross product;
+    Vector3 operator *= (Vector3 v2){
+        Vector3 v3(0,0,0);
+        v3.x = this->y*v2.z-this->z*v2.y;
+        v3.y = -(this->x*v2.z-this->z*v2.x);
+        v3.z = this->x*v2.y-this->y*v2.x;
+        return v3;
+    }
 };
 
