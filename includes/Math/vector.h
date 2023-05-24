377f81dd5d9b6a3d5323c7eacc598769123e527a
Shivang Pandey <shivangp20@iitk.ac.in>
2023-05-24 06:17:40 +0530

    add vector.h and main.cpp

--- includes/Math/vector.h
+++ includes/Math/vector.h
@@ -1,6 +1,7 @@
+
 #pragma once
 #include <iostream>
-
+#include <cmath>
 class Vector3
 {
     public:
@@ -11,7 +12,7 @@ class Vector3
     Vector3(){};
     ~Vector3(){};
 
-    Vector3(float x, float y, float z)
+    Vector3(double x, double y, double z)
     {
         this->x = x;
         this->y = y;
@@ -22,8 +23,62 @@ class Vector3
     {
         std::cout<< "X: " << this->x <<" Y: "<< this->y <<" Z: "<< this->z <<std::endl;
     }
-
-    // Start Your Assignment Here.
-
+     
+    double length()
+    {
+        double l=sqrt(x*x+y*y+z*z);
+        return l;
+    }
+    Vector3 operator* (double s)
+    {
+        Vector3 v2;
+        v2.x=x*s;
+        v2.y=y*s;
+        v2.z=z*s;
+        return v2;
+    }
+    double operator* (Vector3 v1)
+    {
+        double dot;
+        dot=v1.x*(x)+v1.y*(y)+v1.z*(z);
+        return dot;
+    }
+    Vector3 operator+(Vector3 v1)
+    {
+        Vector3 v2;
+        v2.x=x+v1.x;
+        v2.y=y+v1.y;
+        v2.z=z+v1.z;
+        return v2;
+    }
+    Vector3 operator-(Vector3 v1)
+    {
+        Vector3 v2;
+        v2.x=x-v1.x;
+        v2.y=y-v1.y;
+        v2.z=z-v1.z;
+        return v2;
+    }
+    Vector3 operator/(double s)
+    {
+        Vector3 v1;
+        v1.x=x/s;
+        v1.y=y/s;
+        v1.z=z/s;
+        return v1;
+    }
+    double dot(Vector3 v1)
+    {
+        double dot;
+        dot=v1.x*(x)+v1.y*(y)+v1.z*(z);
+        return dot;
+    }
+    Vector3 cross(Vector3 v1)
+    {
+        Vector3 v2;
+        v2.x=x*v1.z-z*v1.y;
+        v2.y=z*v1.x-x*v1.z;
+        v2.z=x*v1.y-y*v1.x;
+        return v2;
+    }
 };
-
--- main.cpp
+++ main.cpp
@@ -1,12 +1,35 @@
 #include <iostream>
 #include "vector.h"
-
+using namespace std;
 int main()
 {
     std::cout<<"Assignment One Evaluation"<<std::endl;
 
     Vector3 vec1(1,2,3);
+    Vector3 vec2(3,4,5);
+    Vector3 vec3;
+    
     vec1.print();
-    std::cin.get();
+
+    cout<<"Length="<<vec1.length()<<endl;
+
+    vec3=vec1+vec2;
+    cout<<"Sum=";
+    vec3.print();
+
+    vec3=vec1-vec2;
+    cout<<"Difference=";
+    vec3.print();   
+
+    cout<<"Dot product="<<vec1.dot(vec2)<<endl;
+    cout<<"Dot product="<<vec1*vec2<<endl;
+
+    vec3=vec1*2;
+    cout<<"Scalar multipication=";
+    vec3.print();
+
+    vec3=vec1/2;
+    cout<<"Scalar division=";
+    vec3.print();
     return 0;
 }
