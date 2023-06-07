2f1ca5e7d149273529c0e65cdf69c43fdb183e34
swapnilbagde0000 <75379349+swapnilbagde0000@users.noreply.github.com>
2023-06-07 22:26:13 +0530

    Add files via upload
    Yo

---
+++ build/CMakeFiles/mfcg.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/b27ad456-8e4b-4dfb-af1e-f8904d3cac76/prj/grendering/assignment2/mfcg2023-Assignment_2/main1.cpp" "CMakeFiles/mfcg.dir/main1.cpp.o" "gcc" "CMakeFiles/mfcg.dir/main1.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
