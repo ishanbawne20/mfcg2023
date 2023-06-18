632a064a12be322aeecba5057158fd224263ab43
prakharpratapmall <prakharpratapmall@gmail.com>
2023-06-18 20:00:00 +0530

    add refract

--- includes/vec3.h
+++ includes/vec3.h
@@ -52,14 +52,11 @@ class vec3 {
         inline static vec3 random(double min, double max) {
             return vec3(random_double(min,max), random_double(min,max), random_double(min,max));
         }
-
-        static vec3 random_in_unit_sphere() {
-            while (true) {
-                auto p = vec3::random(-1,1);
-                if (p.length_squared() >= 1) continue;
-                return p;
-            }
-        }
+        bool near_zero() const {
+        // Return true if the vector is close to zero in all dimensions.
+        const auto s = 1e-8;
+        return (fabs(e[0]) < s) && (fabs(e[1]) < s) && (fabs(e[2]) < s);
+    }
 
     public:
         double e[3];
@@ -115,6 +112,27 @@ inline vec3 unit_vector(vec3 v) {
     return v / v.length();
 }
 
+vec3 reflect(const vec3& v, const vec3& n) {
+    return v - 2*dot(v,n)*n;
+}
 
+vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat) {
+    auto cos_theta = fmin(dot(-uv, n), 1.0);
+    vec3 r_out_perp =  etai_over_etat * (uv + cos_theta*n);
+    vec3 r_out_parallel = -sqrt(fabs(1.0 - r_out_perp.length_squared())) * n;
+    return r_out_perp + r_out_parallel;
+}
+
+vec3 random_in_unit_sphere() {
+            while (true) {
+                auto p = vec3::random(-1,1);
+                if (p.length_squared() >= 1) continue;
+                return p;
+            }
+        }
+
+vec3 random_unit_vector() {
+    return unit_vector(random_in_unit_sphere());
+}
 
 #endif
