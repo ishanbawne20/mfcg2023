410e00e0d1e62d59df531b55e6848e494b68af2e
anuragarmando <95515018+anuragarmando@users.noreply.github.com>
2023-07-13 15:14:33 +0530

    Assignment 1

--- includes/Math/vector.h
+++ includes/Math/vector.h
@@ -1,15 +1,15 @@
 #pragma once
 #include <iostream>
-
+#include<cmath>
 class Vector3
 {
-    public:
+public:
     float x = 0;
     float y = 0;
     float z = 0;
 
-    Vector3(){};
-    ~Vector3(){};
+    Vector3() {}
+    ~Vector3() {}
 
     Vector3(float x, float y, float z)
     {
@@ -20,10 +20,47 @@ class Vector3
 
     void print()
     {
-        std::cout<< "X: " << this->x <<" Y: "<< this->y <<" Z: "<< this->z <<std::endl;
+        std::cout << "X: " << this->x << " Y: " << this->y << " Z: " << this->z << std::endl;
     }
 
-    // Start Your Assignment Here.
+    double length() const
+    {
+        return sqrt(x * x + y * y + z * z);
+    }
 
+    Vector3 operator*(float scalar) const
+    {
+        return Vector3(x * scalar, y * scalar, z * scalar);
+    }
+
+    double operator*(const Vector3& other) const
+    {
+        return x * other.x + y * other.y + z * other.z;
+    }
+
+    Vector3 crossProduct(const Vector3& other) const
+    {
+        return Vector3(y * other.z - z * other.y, z * other.x - x * other.z, x * other.y - y * other.x);
+    }
+
+    Vector3 operator+(const Vector3& other) const
+    {
+        return Vector3(x + other.x, y + other.y, z + other.z);
+    }
+
+    Vector3 operator/(float scalar) const
+    {
+        if (scalar == 0)
+        {
+            std::cout << "Error: Division by zero!" << std::endl;
+            return *this;
+        }
+
+        return Vector3(x / scalar, y / scalar, z / scalar);
+    }
+
+    Vector3 operator-(const Vector3& other) const
+    {
+        return Vector3(x - other.x, y - other.y, z - other.z);
+    }
 };
-
